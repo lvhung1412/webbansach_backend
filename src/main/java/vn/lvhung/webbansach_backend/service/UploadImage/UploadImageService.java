6e222570c86b36bfabec7b7950de1a08a0cebeb0
Lại Văn Hùng <laivanhung1412@gmail.com>
2024-12-24 14:46:12 +0700

    update uploadImage

--- src/main/java/vn/lvhung/webbansach_backend/service/UploadImage/UploadImageService.java
+++ src/main/java/vn/lvhung/webbansach_backend/service/UploadImage/UploadImageService.java
@@ -1,2 +1,8 @@
-package vn.lvhung.webbansach_backend.service.UploadImage;public interface UploadImageService {
+package vn.lvhung.webbansach_backend.service.UploadImage;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface UploadImageService {
+    String uploadImage(MultipartFile multipartFile, String name);
+    void deleteImage(String imgUrl);
 }
