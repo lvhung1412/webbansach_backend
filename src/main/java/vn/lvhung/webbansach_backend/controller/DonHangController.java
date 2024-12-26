0f2dd82c7070d7ea1f68811f146caac9e991dc3a
Lại Văn Hùng <laivanhung1412@gmail.com>
2024-12-27 00:28:19 +0700

    update đơn hàng controller

--- src/main/java/vn/lvhung/webbansach_backend/controller/DonHangController.java
+++ src/main/java/vn/lvhung/webbansach_backend/controller/DonHangController.java
@@ -4,17 +4,18 @@ import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import vn.lvhung.webbansach_backend.service.donhang.DonHangService;
 import vn.lvhung.webbansach_backend.service.giohang.GioHangService;
 
 @RestController
 @RequestMapping("/order")
 public class DonHangController {
     @Autowired
-    private GioHangService gioHangService;
+    private DonHangService donHangService;
     @PostMapping("/add-order")
     public ResponseEntity<?> save (@RequestBody JsonNode jsonData) {
         try{
-            return gioHangService.save(jsonData);
+            return donHangService.save(jsonData);
         } catch (Exception e) {
             e.printStackTrace();
             return ResponseEntity.badRequest().build();
@@ -24,20 +25,20 @@ public class DonHangController {
     @PutMapping("/update-order") // update các trạng thái
     public ResponseEntity<?> update (@RequestBody JsonNode jsonData) {
         try{
-            return gioHangService.update(jsonData);
+            return donHangService.update(jsonData);
         } catch (Exception e) {
             e.printStackTrace();
             return ResponseEntity.badRequest().build();
         }
     }
 
-//    @PutMapping("/cancel-order") // khi thanh toán mà huỷ thanh toán
-//    public ResponseEntity<?> cancle (@RequestBody JsonNode jsonNode) {
-//        try{
-//            return gioHangService.(jsonNode);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            return ResponseEntity.badRequest().build();
-//        }
-//    }
+    @PutMapping("/cancel-order") // khi thanh toán mà huỷ thanh toán
+    public ResponseEntity<?> cancle (@RequestBody JsonNode jsonNode) {
+        try{
+            return donHangService.cancel(jsonNode);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().build();
+        }
+    }
 }
