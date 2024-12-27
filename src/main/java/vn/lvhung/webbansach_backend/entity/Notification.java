d064a034172e68395d346d153c8c6d664f26990d
Lại Văn Hùng <laivanhung1412@gmail.com>
2024-12-27 13:52:47 +0700

    update entity

---
+++ src/main/java/vn/lvhung/webbansach_backend/entity/Notification.java
@@ -0,0 +1,17 @@
+package vn.lvhung.webbansach_backend.entity;
+
+public class Notification {
+    private String message;
+
+    public Notification(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
