b2c5a95133add46523af4144bef9be801cefe816
Lại Văn Hùng <laivanhung1412@gmail.com>
2024-12-27 13:20:59 +0700

    rename path thành deliveries

--- src/main/java/vn/lvhung/webbansach_backend/dao/VanChuyenRepository.java
+++ src/main/java/vn/lvhung/webbansach_backend/dao/VanChuyenRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import vn.lvhung.webbansach_backend.entity.VanChuyen;
 
-@RepositoryRestResource(path = "van-chuyen")
+@RepositoryRestResource(path = "deliveries")
 public interface VanChuyenRepository extends JpaRepository<VanChuyen, Integer> {
 }
