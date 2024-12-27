a23ae5e4aacaab8bd209a0e499d0d867f3b9b4d0
Lại Văn Hùng <laivanhung1412@gmail.com>
2024-12-27 13:23:18 +0700

    rename rest path

--- src/main/java/vn/lvhung/webbansach_backend/dao/TheLoaiRepository.java
+++ src/main/java/vn/lvhung/webbansach_backend/dao/TheLoaiRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import vn.lvhung.webbansach_backend.entity.TheLoai;
 
-@RepositoryRestResource(path = "the-loai")
+@RepositoryRestResource(path = "genre")
 public interface TheLoaiRepository extends JpaRepository<TheLoai,Integer> {
 }
