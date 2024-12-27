a03ab3d1096117538ba8f3531c7b5d3a5962c598
Lại Văn Hùng <laivanhung1412@gmail.com>
2024-12-27 13:22:36 +0700

    đổi tên path

--- src/main/java/vn/lvhung/webbansach_backend/dao/PhanHoiRepository.java
+++ src/main/java/vn/lvhung/webbansach_backend/dao/PhanHoiRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import vn.lvhung.webbansach_backend.entity.PhanHoi;
 
-@RepositoryRestResource(path = "phan-hoi")
+@RepositoryRestResource(path = "feedbacks")
 public interface PhanHoiRepository extends JpaRepository<PhanHoi, Integer> {
     long countBy();
 }
