363ede6da6574fc6cdfbf2fd21f95a3202b60aaf
Lại Văn Hùng <laivanhung1412@gmail.com>
2024-12-27 13:29:46 +0700

    rename payments

--- src/main/java/vn/lvhung/webbansach_backend/dao/ThanhToanRepository.java
+++ src/main/java/vn/lvhung/webbansach_backend/dao/ThanhToanRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import vn.lvhung.webbansach_backend.entity.ThanhToan;
 
-@RepositoryRestResource(path = "thanh-toan")
+@RepositoryRestResource(path = "payments")
 public interface ThanhToanRepository extends JpaRepository<ThanhToan, Integer> {
 }
