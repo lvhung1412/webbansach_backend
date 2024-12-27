becbf05f0754e83dd9fc10e8048fbbd8f90df0dc
Lại Văn Hùng <laivanhung1412@gmail.com>
2024-12-27 13:33:25 +0700

    rename users

--- src/main/java/vn/lvhung/webbansach_backend/dao/NguoiDungRepository.java
+++ src/main/java/vn/lvhung/webbansach_backend/dao/NguoiDungRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import vn.lvhung.webbansach_backend.entity.NguoiDung;
 
-@RepositoryRestResource(excerptProjection = NguoiDung.class,path = "nguoi-dung")
+@RepositoryRestResource(excerptProjection = NguoiDung.class,path = "users")
 public interface NguoiDungRepository extends JpaRepository<NguoiDung,Integer> {
     public boolean existsByTenDangNhap(String tenDangNhap);
 
