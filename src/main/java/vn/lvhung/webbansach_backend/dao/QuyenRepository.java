8fe4b127d80a33a1bba6189160403d0101ee4fa9
Lại Văn Hùng <laivanhung1412@gmail.com>
2024-12-27 13:30:52 +0700

    rename roles

--- src/main/java/vn/lvhung/webbansach_backend/dao/QuyenRepository.java
+++ src/main/java/vn/lvhung/webbansach_backend/dao/QuyenRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import vn.lvhung.webbansach_backend.entity.Quyen;
 
-@RepositoryRestResource(path = "quyen")
+@RepositoryRestResource(path = "roles")
 public interface QuyenRepository extends JpaRepository<Quyen,Integer> {
 
     public  Quyen findByTenQuyen(String tenQuyen);
